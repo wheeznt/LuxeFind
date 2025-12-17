3dfd98e31bc76e85687217c6128e3c596780b159
wheeznt <azcampoos@gmail.com>
2025-12-17 16:14:06 +0700

    benerin full code

--- LuxeFind/LuxeFind2.0.py
+++ LuxeFind/LuxeFind2.0.py
@@ -56,9 +56,9 @@ def buat_post():
 # BERANDA SEPERTI E COMMERCE
 
 def beranda():
-    global current_user
     while True:
-        print("\n=== BERANDA ===")
+        print("
+=== BERANDA ===")
 
         if not posts:
             print("Belum ada permintaan barang")
@@ -66,13 +66,15 @@ def beranda():
             for i, p in enumerate(posts):
                 print(f"{i}. {p['nama']} | {p['merek']} | {p['tahun']}")
 
-        print("\nMenu")
+        print("
+Menu")
         print("1 Lihat detail permintaan")
 
         if users[current_user]["role"] == "user":
             print("2 Buat permintaan barang")
-
-        print("0 Kembali ke Menu Awal")
+            print("3 Logout")
+        else:
+            print("2 Logout")
 
         pilih = input("Pilih menu: ")
 
@@ -80,9 +82,11 @@ def beranda():
             lihat_detail()
         elif pilih == "2" and users[current_user]["role"] == "user":
             buat_post()
+        elif pilih == "2" and users[current_user]["role"] == "mediator":
+            logout()
             return
-        elif pilih == "0":
-            current_user = None
+        elif pilih == "3":
+            logout()
             return
         else:
             print("Pilihan tidak valid")
@@ -100,14 +104,28 @@ def lihat_detail():
 
     p = posts[idx]
     while True:
-        print("\n=== DETAIL PERMINTAAN ===")
+        print("
+=== DETAIL PERMINTAAN ===")
         print("Nama barang:", p["nama"])
         print("Merek:", p["merek"])
         print("Tahun:", p["tahun"])
         print("Deskripsi:", p["deskripsi"])
         print("Kontak user:", p["kontak"])
 
+        print("
+Menu")
+        print("1 Kembali ke beranda")
 
+        pilih = input("Pilih menu: ")
+        if pilih == "1":
+            return
+
+# LOGOUT
+
+def logout():
+    global current_user
+    current_user = None
+    print("Logout berhasil")
 
 # MENU AWAL
 
